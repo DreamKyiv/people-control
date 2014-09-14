7b3576e54994aa0fb325a4f133c5040bacda60d7
Vitaliy Pylypiv <pylypiv@integramedia.eu>
2014-09-14 10:00:04 +0300

    Version is changed

--- people-control.php
+++ people-control.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: DreamKyiv People Control
-Version: 0.0.2
+Version: 0.0.3
 Description: Plugin for Kyiv rada deputies control
 Author: Vitaliy Pylypiv
 GitHub Plugin URI: https://github.com/DreamKyiv/people-control
